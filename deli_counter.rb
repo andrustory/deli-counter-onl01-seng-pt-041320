22cddd799100e98020f7f2ccf15dc22dc3a52146
Andres Carvajal <jc1993box@hotmail.com>
2020-04-24 20:59:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (deli)
   if deli.length == 0
     puts "The line is currently empty." 
   else 
-    string = "The line is currently: "
+    string = "The line is currently:"
     deli.each.with_index(1)do|person, index|
     string << " #{index}. #{person}"
   end
