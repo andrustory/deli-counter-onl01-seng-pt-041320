8bb7b682876f8d54164789858002d0e525b973d1
Andres Carvajal <jc1993box@hotmail.com>
2020-04-24 20:50:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+require 'pry'
+def line (deli)
+  if deli.length == 0
+    puts "The line is currently empty." 
+  else 
+    string = "The line is currently: "
+    katz_deli.each_with_index(1)do|customer, index|
+    string << "#{index+1}. #{customer}"
+  end
+  string
+end
+end
